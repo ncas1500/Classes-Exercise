55bee600ffcf5d766e3bd426a0207c6388131f47
narciso castro <narcisocastro0802@gmail.com>
2024-06-25 14:23:21 -0700

    Added classes to program Car

---
+++ Classes/Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class Car
+    {
+       public string Make {  get; set; }
+       public string Model { get; set; }
+       public int Year { get; set; }
+
+       
+    }
+}
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace Classes
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Classes
 {
-    public class Program
+     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var fusion = new Car();
+            fusion.Make = "Ford";
+            fusion.Model = "Fusion";
+            fusion.Year = 2019;
+
+            Console.WriteLine($"{fusion.Make}, {fusion.Model}, {fusion.Year}");
         }
     }
 }
